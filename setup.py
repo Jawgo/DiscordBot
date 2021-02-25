be2a5bb870445fd458b1b7dc8daa470fbe5cfa87
Jawgo <jmschijn@ucalgary.ca>
2021-02-25 14:21:42 -0700

    adding lxml

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    install_requires=['requests', 'discord', 'bs4', 'simplejson', 'selenium' ],
+    install_requires=['requests', 'discord', 'bs4', 'simplejson', 'selenium', 'lxml' ],
 
     classifiers=[
         'Natural Language :: English',
