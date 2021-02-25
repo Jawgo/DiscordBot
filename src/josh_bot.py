23bcef398446cde19bc69aadfef8ff3a2a7f2757
Jawgo <jmschijn@ucalgary.ca>
2021-02-24 21:15:31 -0700

    more updates, slowly we shall bring it alive

--- src/josh_bot.py
+++ src/josh_bot.py
@@ -7,22 +7,19 @@ import discord
 import os
 
 class DiscordBot(object):
-    client = discord.Client()
 
     def __init__(self, token):
+        self._client = discord.Client()
         self._token = token
 
     def start(self):
-        self.client.run(self._token)
+        self._client.run(self._token)
 
-
-    @client.event
     async def on_ready(self):
-        print('We have logged in as {0.user}'.format(self.client))
+        print('We have logged in as {0.user}'.format(self._client))
 
-    @client.event
     async def on_message(self, message):
-        if message.author == self.client.user:
+        if message.author == self._client.user:
             return
 
         if message.content.startswith('$hello'):
