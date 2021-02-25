a4fa8c76cf247d4b9b6fafc8d38e9aba7de392d2
Jawgo <jmschijn@ucalgary.ca>
2021-02-25 14:39:51 -0700

    add chrome option for disabling blink features

--- src/url_requester.py
+++ src/url_requester.py
@@ -15,6 +15,7 @@ def check_avail():
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options..add_argument("--disable-blink-features=AutomationControlled")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     chrome_options.add_argument("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36")
